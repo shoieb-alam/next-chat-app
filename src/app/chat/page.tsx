2b55a4f81f4d798252bde773f6b0cd7d19d60684
Shoieb Alam <shoieb-alam@users.noreply.github.com>
2024-12-06 21:35:03 +0600

    fix chat window height

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -8,11 +8,11 @@ export default function ChatPage() {
   const [currentRoom, setCurrentRoom] = useState("General");
 
   return (
-    <main className="chat-container flex min-h-screen">
+    <main className="chat-container flex min-h-[90vh]">
       {/* <div className="room-list w-64 bg-gray-200 p-4"> */}
       <RoomList onSelectRoom={(room) => setCurrentRoom(room)} />
       {/* </div> */}
-      <div className="chat-window flex-1 bg-white p-4 shadow-lg rounded-lg">
+      <div className="chat-window flex-1 bg-white p-2 shadow-lg rounded-lg max-h-[90vh]">
         <ChatWindow room={currentRoom} />
       </div>
     </main>
